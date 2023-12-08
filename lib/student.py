e2b825c4b0cd50e61fc652faf6eda6f60de013cb
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-08 11:50:33 +0300

    Assignment

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,17 @@
 class Student:
-    pass
+    def hello(self):
+        print("Hello, I'm a student.")
+
+    def raise_hand(self):
+        print("Raising hand respectfully to get the teacher's attention.")
+
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        print("Hello, I'm a chatty student. Did you watch the latest TV show?")
+
+    def raise_hand(self):
+        for _ in range(10):
+            print("Raising hand respectfully to get the teacher's attention.")
+
+
