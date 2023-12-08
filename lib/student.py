a242ee7a05eeb7abe261ddfe338f840052c1b352
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-08 12:00:40 +0300

    Assignment

--- lib/student.py
+++ lib/student.py
@@ -1,17 +1,19 @@
 class Student:
     def hello(self):
-        print("Hello, I'm a student.")
+        print("Hey there! I'm so excited to learn stuff.")
 
     def raise_hand(self):
-        print("Raising hand respectfully to get the teacher's attention.")
+        print("Pick me!")
 
 
 class ChattyStudent(Student):
     def hello(self):
-        print("Hello, I'm a chatty student. Did you watch the latest TV show?")
+        super().hello() 
+        print(
+            "How are you doing today? I'm okay, but I'm kind of tired.Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
 
     def raise_hand(self):
         for _ in range(10):
-            print("Raising hand respectfully to get the teacher's attention.")
-
+            super().raise_hand()
+            
 
